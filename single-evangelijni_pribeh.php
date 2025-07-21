2abc7c0b556be974f3484ed8b339fa3dfd2c30e7
Pehobr <pehobr@gmail.com>
2025-07-21 05:55:42 +0200

    Přihlašování - ladění

--- single-evangelijni_pribeh.php
+++ single-evangelijni_pribeh.php
@@ -1,12 +1,12 @@
 <?php
 get_header();
 
-// Zjistíme, zda má uživatel přístup k prémiovému obsahu
-$is_access_granted = knihaslova_is_access_granted();
+// Zjistíme, zda má uživatel přístup k neveřejnému obsahu
+$is_access_granted = function_exists('knihaslova_is_access_granted') && knihaslova_is_access_granted();
 
 // Základní data o příběhu
 $post_slug = get_post_field( 'post_name', get_the_ID() );
-$story_data = knihaslova_get_story_data($post_slug);
+$story_data = function_exists('knihaslova_get_story_data') ? knihaslova_get_story_data($post_slug) : [];
 
 // Načtení dat pro jednotlivé záložky
 $comparison_page_slug = $post_slug . '-srovnani';
@@ -18,7 +18,7 @@ $parafraze_page = get_page_by_path($parafraze_page_slug, OBJECT, 'page');
 $podcast_page_slug = $post_slug . '-podcast';
 $podcast_page = get_page_by_path($podcast_page_slug, OBJECT, 'page');
 
-// --- NOVINKA: Načtení stránky pro záložku "Pro kněze" ---
+// Načtení stránky pro záložku "Pro kněze"
 $pro_kneze_page_slug = $post_slug . '-pro-kneze';
 $pro_kneze_page = get_page_by_path($pro_kneze_page_slug, OBJECT, 'page');
 ?>
@@ -38,23 +38,21 @@ $pro_kneze_page = get_page_by_path($pro_kneze_page_slug, OBJECT, 'page');
                     <button class="nav-tab" data-target="translations-view">Překlady</button>
                     <button class="nav-tab" data-target="analysis-view">Exegeze</button>
                     <button class="nav-tab" data-target="spiritual-view">Výklad</button>
+                    
+                    <?php // --- Tyto záložky jsou nyní VŽDY viditelné --- ?>
+                    <?php if ($comparison_page): ?>
+                        <button class="nav-tab" data-target="text-comparison-view">Srovnání</button>
+                    <?php endif; ?>
+                    <?php if ($parafraze_page): ?>
+                        <button class="nav-tab" data-target="parafraze-view">Spojení</button>
+                    <?php endif; ?>
+                    <?php if ($podcast_page): ?>
+                        <button class="nav-tab" data-target="podcast-view">Podcast</button>
+                    <?php endif; ?>
 
-                    <?php // --- Chráněné záložky (zobrazí se pouze po přihlášení) --- ?>
-                    <?php if ($is_access_granted): ?>
-                        <?php if ($comparison_page): ?>
-                            <button class="nav-tab" data-target="text-comparison-view">Srovnání</button>
-                        <?php endif; ?>
-                        <?php if ($parafraze_page): ?>
-                            <button class="nav-tab" data-target="parafraze-view">Spojení</button>
-                        <?php endif; ?>
-                        <?php if ($podcast_page): ?>
-                            <button class="nav-tab" data-target="podcast-view">Podcast</button>
-                        <?php endif; ?>
-                        
-                        <?php // --- NOVINKA: Tlačítko pro záložku "Pro kněze" --- ?>
-                        <?php if ($pro_kneze_page): ?>
-                            <button class="nav-tab" data-target="pro-kneze-view">Pro kněze</button>
-                        <?php endif; ?>
+                    <?php // --- Chráněná záložka (zobrazí se pouze po přihlášení) --- ?>
+                    <?php if ($is_access_granted && $pro_kneze_page): ?>
+                        <button class="nav-tab" data-target="pro-kneze-view">Pro kněze</button>
                     <?php endif; ?>
                 </div>
 
@@ -72,43 +70,39 @@ $pro_kneze_page = get_page_by_path($pro_kneze_page_slug, OBJECT, 'page');
                     <?php set_query_var('story_data', $story_data); set_query_var('post_slug', $post_slug); get_template_part('template-parts/content-spiritual'); ?>
                 </div>
 
-                <?php // --- Obsah chráněných záložek (zobrazí se pouze po přihlášení) --- ?>
-                <?php if ($is_access_granted): ?>
-                    <?php if ($comparison_page): ?>
-                        <div id="text-comparison-view" class="tab-content">
-                            <div class="analysis-content content-to-copy-wrapper">
-                                <button class="copy-to-clipboard-btn" title="Zkopírovat text" data-clipboard-target="#text-comparison-view .copy-target"><i class="fa fa-clone" aria-hidden="true"></i></button>
-                                <div class="copy-target"><?php echo apply_filters('the_content', $comparison_page->post_content); ?></div>
-                            </div>
+                <?php // --- Obsah nyní veřejných záložek --- ?>
+                <?php if ($comparison_page): ?>
+                    <div id="text-comparison-view" class="tab-content">
+                        <div class="analysis-content content-to-copy-wrapper">
+                            <button class="copy-to-clipboard-btn" title="Zkopírovat text" data-clipboard-target="#text-comparison-view .copy-target"><i class="fa fa-clone" aria-hidden="true"></i></button>
+                            <div class="copy-target"><?php echo apply_filters('the_content', $comparison_page->post_content); ?></div>
                         </div>
-                    <?php endif; ?>
+                    </div>
+                <?php endif; ?>
 
-                    <?php if ($parafraze_page): ?>
-                        <div id="parafraze-view" class="tab-content">
-                            <div class="analysis-content content-to-copy-wrapper">
-                                <button class="copy-to-clipboard-btn" title="Zkopírovat text" data-clipboard-target="#parafraze-view .copy-target"><i class="fa fa-clone" aria-hidden="true"></i></button>
-                                <div class="copy-target"><?php echo apply_filters('the_content', $parafraze_page->post_content); ?></div>
-                            </div>
+                <?php if ($parafraze_page): ?>
+                    <div id="parafraze-view" class="tab-content">
+                        <div class="analysis-content content-to-copy-wrapper">
+                            <button class="copy-to-clipboard-btn" title="Zkopírovat text" data-clipboard-target="#parafraze-view .copy-target"><i class="fa fa-clone" aria-hidden="true"></i></button>
+                            <div class="copy-target"><?php echo apply_filters('the_content', $parafraze_page->post_content); ?></div>
                         </div>
-                    <?php endif; ?>
+                    </div>
+                <?php endif; ?>
 
-                    <?php if ($podcast_page): ?>
-                        <div id="podcast-view" class="tab-content">
-                            <div class="podcast-player-container"><?php echo apply_filters('the_content', $podcast_page->post_content); ?></div>
+                <?php if ($podcast_page): ?>
+                    <div id="podcast-view" class="tab-content">
+                        <div class="podcast-player-container"><?php echo apply_filters('the_content', $podcast_page->post_content); ?></div>
+                    </div>
+                <?php endif; ?>
+                
+                <?php // --- Obsah chráněné záložky (zobrazí se pouze po přihlášení) --- ?>
+                <?php if ($is_access_granted && $pro_kneze_page): ?>
+                    <div id="pro-kneze-view" class="tab-content">
+                        <div class="analysis-content content-to-copy-wrapper">
+                            <button class="copy-to-clipboard-btn" title="Zkopírovat text" data-clipboard-target="#pro-kneze-view .copy-target"><i class="fa fa-clone" aria-hidden="true"></i></button>
+                            <div class="copy-target"><?php echo apply_filters('the_content', $pro_kneze_page->post_content); ?></div>
                         </div>
-                    <?php endif; ?>
-
-                    <?php // --- NOVINKA: Obsah pro záložku "Pro kněze" --- ?>
-                    <?php if ($pro_kneze_page): ?>
-                        <div id="pro-kneze-view" class="tab-content">
-                            <div class="analysis-content content-to-copy-wrapper">
-                                <button class="copy-to-clipboard-btn" title="Zkopírovat text" data-clipboard-target="#pro-kneze-view .copy-target"><i class="fa fa-clone" aria-hidden="true"></i></button>
-                                <div class="copy-target">
-                                    <?php echo apply_filters('the_content', $pro_kneze_page->post_content); ?>
-                                </div>
-                            </div>
-                        </div>
-                    <?php endif; ?>
+                    </div>
                 <?php endif; ?>
 
             </div>
@@ -119,4 +113,3 @@ $pro_kneze_page = get_page_by_path($pro_kneze_page_slug, OBJECT, 'page');
 
 <?php
 get_footer();
-?>
